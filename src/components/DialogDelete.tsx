79b48ead0cd88a92d44c998a2b840a9ee3e92f56
sarita <bsarita926@gmail.com>
2024-10-25 15:59:58 +0545

    feat: create route for delete dialog

---
+++ src/components/DialogDelete.tsx
@@ -0,0 +1,81 @@
+// ** React Imports
+import {Fragment, useEffect, useState} from 'react'
+
+// ** MUI Imports
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+
+
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    Typography
+} from '@mui/material'
+import toast, { Toaster } from 'react-hot-toast'
+import Box from "@mui/material/Box";
+import DialogActions from "@mui/material/DialogActions";
+import {Icon} from "@iconify/react";
+
+
+
+
+const DialogDelete = ({ show, handleDialog ,handleDelete}) => {
+    const handleClick=()=>{
+        // handleDelete()
+        handleDialog(false)
+    }
+
+    return (
+
+        <Fragment>
+            <Dialog
+                open={show}
+                maxWidth='xs'
+                sx={{'& .MuiDialog-paper': {overflow: 'visible'}}}
+
+            >
+                <DialogTitle id='alert-dialog-title'></DialogTitle>
+                <DialogContent
+
+                >
+
+                    <Toaster position='top-right' reverseOrder={false}/>
+
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            textAlign: 'center',
+                            alignItems: 'center',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            '& svg': {  color: 'warning.main' }
+                        }}
+                    >
+                        <Icon icon='tabler:alert-circle' fontSize='3rem' />
+                        <Typography variant='h6' sx={{ mb:1, fontWeight:600 }}>
+                            Are you sure you want to delete?
+                        </Typography>
+                    </Box>
+                    <Box sx={{mt:3,display:'flex',gap:3}}>
+                        <Button variant='tonal' color='secondary' onClick={() => handleDialog(false)}>
+                            Cancel
+                        </Button>
+                        <Button variant='contained' sx={{ mr: 3 }} onClick={handleClick}>
+                            Delete
+                        </Button>
+                    </Box>
+                </DialogContent>
+
+
+            </Dialog>
+        </Fragment>
+
+
+    )
+}
+
+
+export default DialogDelete
--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,9 +1,46 @@
 "use client"
 import {Box, Button, Card, CardContent, CardHeader, IconButton, Typography} from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
-const MessageCard=()=>{
+import {useState} from "react";
+import DialogDelete from "@/components/DialogDelete";
+import {Message} from "@/model/User";
+import {ApiResponse} from "@/types/ApiResponse";
+import axios from "axios";
+import toast, {Toaster} from "react-hot-toast";
+
+type MessageCardContent={
+  message:Message;
+  onMessageDelete:(messageId:string)=>void;
+}
+const MessageCard=({message,onMessageDelete}:MessageCardContent)=>{
+    const [openDeleteDialog,setOpenDeleteDialog]=useState(false)
+    const handleDeleteClick=(show)=>{
+        setOpenDeleteDialog(show)
+    }
+
+  const handleDelete= async ()=>{
+        const response = await axios.delete<ApiResponse>(`api/delete-message/${message._id}`)
+      if(response.data.status)
+      {
+          toast.success(response.data.message)
+      }
+      else {
+          toast.error(response.data.message)
+      }
+      onMessageDelete(message._id)
+  }
+
     return<>
         <Card>
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
+            <DialogDelete
+                show={openDeleteDialog}
+                handleDialog={handleDeleteClick}
+                handleDelete={handleDelete}
+            />
             <CardHeader></CardHeader>
             <CardContent>
                <Box sx={{display:'flex', gap:4}}>
@@ -11,7 +48,7 @@ const MessageCard=()=>{
                        <Typography>Sarita Bist</Typography>
                        <Typography>Mar23, 2024</Typography>
                    </Box>
-                  <Button variant='contained' sx={{backgroundColor:'red'}}> <CloseIcon onClick={()=>console.log("Hello")}></CloseIcon></Button>
+                  <Button variant='contained' onClick={()=>handleDeleteClick(true)} sx={{backgroundColor:'red'}}> <CloseIcon ></CloseIcon></Button>
                </Box>
             </CardContent>
         </Card>
